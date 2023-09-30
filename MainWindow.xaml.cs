a47a45cb40663a3810c69843b8aa69a68384e364
배정훈 <yangkwa@4csoft.com>
2023-10-01 00:36:25 +0900

    배포판 만들기

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 
 namespace LX2CBTWin
 {
@@ -23,6 +24,31 @@ namespace LX2CBTWin
         public MainWindow()
         {
             InitializeComponent();
+
+            mainTitle.Content = ConfigurationManager.AppSettings["Title"];
+          
+        }
+
+        private async void webView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await webView.EnsureCoreWebView2Async();
+
+            webView.Source = new Uri("https://lx2.kr");
+
+            webView.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = false;
+            webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+
+        }
+
+        private void MainWindow_Deactivated(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Topmost = true;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
